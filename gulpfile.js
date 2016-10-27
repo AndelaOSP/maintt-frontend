e6b01776625d3501882f1f1d9e78fdf36bdc439f
Thomas Nyambati <thomas.nyambati@andela.com>
2016-10-27 16:04:23 +0100

    chore(ng): Install angular-cli   - install angular-cli globally for ng build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,3 @@
 'use strict';
 const gulp = require('gulp');
-const { task } = require('gulp-shell');
 require('require-dir')('./tasks/');
-
-gulp.task('heroku:staging', ['bower'], task(['ng build --prod']));
-gulp.task('heroku:production', ['bower'], task(['ng build --prod']));
--- tasks/build.js
+++ tasks/build.js
@@ -3,7 +3,14 @@ const gulp = require('gulp');
 const { task } = require('gulp-shell');
 const nodemon = require('gulp-nodemon');
 
-gulp.task('default', ['nodemon', 'bower'], task([
-  'open http://localhost:4200',
-  'npm start'
-]));
+gulp.task('heroku:staging', ['bower'],
+  task(['ng build --prod']));
+
+gulp.task('heroku:production', ['bower'],
+  task(['ng build --prod']));
+
+gulp.task('default', ['nodemon', 'bower'],
+  task([
+    'open http://localhost:4200',
+    'npm start'
+  ]));
