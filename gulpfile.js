d06b1fa65b0aca672fd315b59ab490a7e9081fd9
Thomas Nyambati <thomas.nyambati@andela.com>
2016-10-27 15:56:19 +0100

    chore(heroku): add heroku tasks - add heroku tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
 'use strict';
 const gulp = require('gulp');
+const { task } = require('gulp-shell');
 require('require-dir')('./tasks/');
+
+gulp.task('heroku:staging', ['bower'], task(['ng build --prod']));
+gulp.task('heroku:production', ['bower'], task(['ng build --prod']));
